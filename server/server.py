f2ef965d1f7db515104b8ff0efbcc2d8e774eac8
Dan <d.c.howarth@gmail.com>
2014-01-14 23:58:46 +0000

    WIP

--- server/server.py
+++ server/server.py
@@ -1,12 +1,20 @@
 import cherrypy
+from mako.template import Template
+from mako.lookup import TemplateLookup
+import os.path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+lookup = TemplateLookup(directories=['html'])
 
 class Server(object):
 
     @cherrypy.expose()
     def index(self):
-        return 'noodles'
+        tmpl = lookup.get_template('base.html')
+        return tmpl.render()
 
 cherrypy.config.update({'server.socket_host': '127.0.0.1', 
                          'server.socket_port': 9999, 
                         }) 
-cherrypy.quickstart(Server())
+conf = {'/dist': {'tools.staticdir.on': True, 'tools.staticdir.dir': os.path.join(current_dir, 'dist')}}
+cherrypy.quickstart(Server(), '/', config=conf)
